92c66fa280439e118dad4888575bdce9d00b763f
Fergal Daly <fergald@gmail.com>
2023-08-03 11:05:55 +0900

    close all

--- message-port/weak-ref.js
+++ message-port/weak-ref.js
@@ -1,4 +1,6 @@
 let channel = new WeakRef(new MessageChannel());
+channel.deref().port1.close();
+channel.deref().port2.close();
 setInterval(() => {
   console.log(channel.deref());
   document.body.textContent = channel.deref();
