b3091af057d8612cfca78a0e25782277b5c692a4
Fergal Daly <fergald@gmail.com>
2023-08-14 21:00:41 +0900

    add same-origin case

--- message-port/weak-ref.js
+++ message-port/weak-ref.js
@@ -20,15 +20,41 @@ function setup() {
   };
   document.getElementById("closePort1").onclick = () => { port1.deref().close() };
   document.getElementById("closePort2").onclick = () => { port2.deref().close() };
-  const sendPort1 = document.getElementById("sendPort1");
-  sendPort1.onclick = () => {
+  const sendPort1Cross = document.getElementById("sendPort1Cross");
+  const sendPort1Same = document.getElementById("sendPort1Same");
+  sendPort1Cross.onclick = () => {
+    sendPort1Cross.setAttribute("disabled", "");
+    sendPort1Same.setAttribute("disabled", "");
     const iframe = document.createElement("iframe");
     document.body.appendChild(iframe);
     iframe.src = "https://www.example.com/";
     iframe.contentWindow.postMessage(port1.deref(), "*", [port1.deref()]);
-    sendPort1.textContent = "delete iframe";
-    sendPort1.onclick = () => {
+    const deleteIframe = document.getElementById("deleteIframe");
+    deleteIframe.removeAttribute("disabled");
+    deleteIframe.onclick = () => {
       document.body.removeChild(iframe);
+      deleteIframe.setAttribute("disabled", "");
+    };
+  };
+
+  let messageHolder;
+
+  sendPort1Same.onclick = () => {
+    sendPort1Cross.setAttribute("disabled", "");
+    sendPort1Same.setAttribute("disabled", "");
+    const iframe = document.createElement("iframe");
+    document.body.appendChild(iframe);
+    iframe.src = "about:blank";
+    iframe.contentWindow.onmessage = message => {
+      console.log("message received");
+      messageHolder = message;
+    };
+    iframe.contentWindow.postMessage(port1.deref(), "*", [port1.deref()]);
+    const dropReference = document.getElementById("dropReference");
+    dropReference.removeAttribute("disabled");
+    dropReference.onclick = () => {
+      messageHolder = null;
+      dropReference.setAttribute("disabled", "");
     };
   };
 }
