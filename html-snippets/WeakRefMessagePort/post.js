2032d80afda532be7d9756b90b421216dd5c083b
Fergal Daly <fergald@gmail.com>
2024-02-20 17:39:38 +0900

    add docs and control

--- html-snippets/WeakRefMessagePort/post.js
+++ html-snippets/WeakRefMessagePort/post.js
@@ -1,14 +1,18 @@
 let { port1, port2 } = new MessageChannel();
 const port1_weak = new WeakRef(port1);
 port1.start();
-port1 = null;
+
+let control = ["a string"];
+const control_weak = new WeakRef(control);
 
 function updateMessage() {
-  document.getElementById("message").textContent = `WeakRef.deref() == ${port1_weak.deref()}`;
+  document.getElementById("message").textContent = `port1_weak.deref() == ${port1_weak.deref()}\ncontrol_weak.deref() == ${control_weak.deref()}`;
 }
 
 function sendPort() {
   w = window.open("about:blank");
   w.postMessage("Transferring message port", "*", [port2]);
+  port1 = null;
+  control = null;
   setInterval(updateMessage, 50);
 }
