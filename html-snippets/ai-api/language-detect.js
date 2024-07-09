5e3df8c9a7b2c93952802a9ad2fb00ff2a291e80
Fergal Daly <fergal@chromium.org>
2024-07-09 02:22:44 +0000

    improve halmet

--- html-snippets/ai-api/language-detect.js
+++ html-snippets/ai-api/language-detect.js
@@ -21,34 +21,52 @@ async function init() {
   setStatus(`canDetect returned: ${can}`);
 
   able(goButton, can != "no");
+  able(hamletButton, can != "no");
 };
 
 init();
 
+async function detect(input) {
+  const detector = await translation.createDetector();
+  input = input.trim();
+  console.log(`Input: ${input}`);
+  const startMs = Date.now();
+  const output = await detector.detect(input);
+  const endMs = Date.now();
+  const durationMs = endMs - startMs;
+  const charsPerMs = input.length / durationMs;
+  console.log(output);
+  response.textContent = `${output.detectedLanguage}: ${Math.floor(output.confidence * 100)}%.
+   ${input.length} characters. Took ${durationMs}ms, ${charsPerMs} characters per ms`;
+}
+
 goButton.onclick = async () => {
   able(goButton, false);
   try {
-    const detector = await translation.createDetector();
-    let input = promptArea.value;
-    input = input.trim();
-    console.log(`Input: ${input}`);
-    const startMs = Date.now();
-    const output = await detector.detect(input);
-    const endMs = Date.now();
-    const durationMs = endMs - startMs;
-    const durationPerMs = durationMs / input.length * 100;
-    console.log(output);
-    response.textContent = `${output.detectedLanguage}: ${Math.floor(output.confidence * 100)}%.
-     ${input.length} characters. Took ${durationMs}ms, ${durationPerMs}ms per 100 characters`;
+    detect(promptArea.value);
   } finally {
     able(goButton, true);
   }
 };
 
 let hamletText;
-hamletButton.onclick = async () => {
+async function getHamlet(needChars) {
   if (!hamletText) {
     hamletText = await fetch("hamlet.txt").then(r => r.text());
   }
-  promptArea.value = hamletText.substr(0, charsInput.value);
+  let hamletSample = "";
+  while (needChars) {
+    hamletSample += hamletText.substr(0, needChars);
+    needChars = charsInput.value - hamletSample.length;
+  }
+  return hamletSample;
+}
+
+hamletButton.onclick = async () => {
+  able(hamletButton, false);
+  try {
+    detect(await getHamlet(charsInput.value));
+  } finally {
+    able(hamletButton, true);
+  }
 }
