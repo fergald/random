03eb7d215aa406856a61401cfc109d20fb400846
Fergal Daly <fergald@gmail.com>
2024-09-25 19:22:56 +0900

    add localstorage

--- html-snippets/speech/index.js
+++ html-snippets/speech/index.js
@@ -1,15 +1,21 @@
+localStorage.setItem("key", "value");
 window.onload = () => {
   const synth = window.speechSynthesis;
+  const ls = window.localStorage;
+
   document.getElementById("speakButton").onclick = () => {
     synth.speak(new SpeechSynthesisUtterance("hello"));
+    console.log("key=", ls.getItem("key"));
   };
 
   const frame1 = document.getElementById("frame1");
   console.log(frame1.contentWindow.synth1);
   const synth1 = frame1.contentWindow.synth1;
+  const ls1 = frame1.contentWindow.localStorage;
   document.getElementById("speakButton1").onclick = () => {
     console.log(synth1);
     synth1.speak(new SpeechSynthesisUtterance("hello"));
+    console.log("key=", ls.getItem("key"));
   };
 
   document.getElementById("removeButton").onclick = () => {
