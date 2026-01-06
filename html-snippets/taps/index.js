8ff7926f1e0109f4d1c252509ec45412f57f55d2
Fergal Daly <fergald@gmail.com>
2026-01-06 16:27:10 +0900

    add button

--- html-snippets/taps/index.js
+++ html-snippets/taps/index.js
@@ -1,5 +1,6 @@
 const tap = document.getElementById("tap");
 const tap2 = document.getElementById("tap2");
+const button = document.getElementById("button");
 const logElem = document.getElementById("log");
 
 function log(message) {
@@ -20,3 +21,6 @@ tap2.onclick = e =>{
   log("single on 2");
 };
 
+button.onclick = e => {
+  log("single on button");
+};
