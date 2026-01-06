035d98f52e9730043bf0afe8d3bc365a1bf2857b
Fergal Daly <fergald@gmail.com>
2026-01-06 16:06:25 +0900

    add tap testing

---
+++ html-snippets/taps/index.js
@@ -0,0 +1,22 @@
+const tap = document.getElementById("tap");
+const tap2 = document.getElementById("tap2");
+const logElem = document.getElementById("log");
+
+function log(message) {
+  console.log(message);
+  const messageElem = document.createElement('pre');
+  messageElem.textContent = message;
+  logElem.appendChild(messageElem);
+}
+tap.onclick = e =>{
+  log("single on 1");
+};
+
+tap2.ondblclick = e =>{
+  log("double");
+};
+
+tap2.onclick = e =>{
+  log("single on 2");
+};
+
