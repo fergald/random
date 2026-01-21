b998f4c60ccf408968927047a87865ded5dc47ec
Fergal Daly <fergald@gmail.com>
2026-01-21 14:07:16 +0900

    snippet to play around with forms file choosers

---
+++ html-snippets/file-select/index.js
@@ -0,0 +1,45 @@
+window.addEventListener("pageshow", e => {
+  bfcacheLogDiv.textContent = `Restored from BFCache = ${e.persisted}`;
+});
+
+function openGoogleTab() {
+  console.log("opening new tab");
+  window.open("https://www.google.com");
+}
+
+function navigateGoogle() {
+  console.log("navigating");
+  window.location = "https://www.google.com";
+}
+
+dialogThenTabButton.onclick = () => {
+  fileInput.click();
+  openGoogleTab();
+}
+
+tabThenDialogButton.onclick = () => {
+  openGoogleTab();
+  fileInput.click();
+};
+
+delayedDialogButton.onclick = () => {
+    setTimeout(() => {
+      fileInput.click();
+    },
+      2000);
+}
+
+delayedTabButton.onclick = () => {
+    setTimeout(() => {
+      openGoogleTab();
+    },
+      2000);
+}
+
+delayedNavigateButton.onclick = () => {
+    setTimeout(() => {
+      navigateGoogle();
+    },
+      2000);
+}
+
