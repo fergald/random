2c1bf8b6bfc55fb47e27ff319aba296229fcf833
Fergal Daly <fergald@gmail.com>
2026-09-01 11:34:28 +0900

    log reasons

--- html-snippets/bfcache-delete-iframe/index.js
+++ html-snippets/bfcache-delete-iframe/index.js
@@ -1,6 +1,11 @@
 addEventListener("pageshow", e => {
   const logDiv = document.getElementById("logPre");
   logDiv.textContent += `Peristed ${e.persisted}\n`;
+  const navEntries = performance.getEntriesByType("navigation");
+  for (const navEntry of navEntries) {
+  logDiv.textContent += `Reasons ${JSON.stringify(navEntry.notRestoredReasons, null, "  ")}\n`;
+  }
+
   const iframeDiv = document.getElementById("iframeDiv");
   const iframe = document.createElement("iframe");
   iframeDiv.appendChild(iframe);
@@ -10,4 +15,3 @@ addEventListener("pagehide", () => {
   const iframeDiv = document.getElementById("iframeDiv");
   iframeDiv.innerHTML = "";
 });
-
